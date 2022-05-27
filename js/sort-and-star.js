87fe166da239e54ad514ca5a1b5f4dd63022edd6
Billy Rodriguez <billyrdz@gmail.com>
2022-05-27 11:05:08 -0500

    sort and star

---
+++ js/sort-and-star.js
@@ -0,0 +1,15 @@
+const instuctions = `You will be given a vector of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+
+The returned value must be a string, and have "***" between each of its letters.
+
+You should not remove or add elements from/to the array.`;
+
+let answer = `function twoSort(s) {
+  return s.sort()[0].split('').join('***')
+ }`;
+
+let arrowAnswer = `const twoSort = s => s.sort()[0].split('').join('***');`;
+
+console.log(instuctions);
+console.log(answer);
+console.log(arrowAnswer);
