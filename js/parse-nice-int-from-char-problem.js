238614f023efdec52b3b5bd0074fd81f70735c2b
Billy Rodriguez <billyrdz@gmail.com>
2022-08-20 13:18:45 -0500

    new challenge

--- js/parse-nice-int-from-char-problem.js
+++ js/parse-nice-int-from-char-problem.js
@@ -5,7 +5,6 @@ Write a program that returns the girl's age (0-9) as an integer.
 Assume the test input string is always a valid string. For example, the test input may be "1 year old" or "5 years old". The first character in the string is always a number.`;
 
 const answer = `function getAge(inputString){
-  // return the girl's correct age as an integer. Happy coding :)
     return parseInt(inputString[0])
   }`;
 
